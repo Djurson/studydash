581a7271325c43cdf1d6553b54bced8387892b60
Emil Djurson <emidj236@student.liu.se>
2025-03-15 22:09:20 +0100

    Klar skapa konto UI (iprincip)

--- src/components/inputfield.tsx
+++ src/components/inputfield.tsx
@@ -1,15 +1,16 @@
 interface InputfieldProps {
-    type: string;
-    name: string;
-    placeholder: string;
-    value: string;
-    onchange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    type?: string;
+    name?: string;
+    placeholder?: string;
+    value?: string;
+    onchange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    classname?: string;
 }
 
-export default function InputField({ type = "", name = "", placeholder = "", value = "", onchange }: InputfieldProps) {
+export default function InputField({ type, name, placeholder, value, onchange, classname }: InputfieldProps) {
     return (
         <input
-            className="flex-1 outline-none bg-white border-2 border-gray-100 text-regular py-3 px-4 placeholder:text-gray-600 rounded-sm text-gray-900"
+            className={`flex-1 outline-none bg-white border-2 border-gray-100 text-regular py-3 px-4 placeholder:text-gray-600 rounded-sm text-gray-900 ${classname}`}
             placeholder={placeholder}
             type={type}
             name={name}
