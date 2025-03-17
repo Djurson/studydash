b621882b0ff368c8ed63238e32823a83910f25ac
Emil Djurson <emidj236@student.liu.se>
2025-03-17 17:03:19 +0100

    Ändrat lite i gitignore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import "./globals.css";
-import { auth } from "../../firebase/client";
 import { AuthProvider } from "@/components/firebase/authcontext";
 
 const inter = Inter({ subsets: ['latin'] })
