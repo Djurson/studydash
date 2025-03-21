faf0ee68b754a21b80efb236f6e8c88a3bab8550
Emil Djurson <emidj236@student.liu.se>
2025-03-21 16:52:42 +0100

    Variant sida
    Lagt till variant för den andra sid typen

--- src/components/PageTemplate.tsx
+++ src/components/PageTemplate.tsx
@@ -4,23 +4,22 @@ import { ClientSidebar } from "@/components/navigation/clientsidebar";
 
 type PageProps = {
   children: ReactNode;
+  defaultPage: boolean;
 };
 
-export function DefaultPageTemplate({ children }: PageProps) {
+export function PageTemplate({ children, defaultPage }: PageProps) {
   return (
-    <>
-      <div className="max-w-screen min-h-screen h-[300vh] bg-green-400 flex">
-        <TopNavBar />
-        <ClientSidebar />
-        <div className="flex justify-center bg-white-400 w-screen">
-          <main className="flex p-4 w-full justify-center mt-[3.688rem] max-w-[82vw]">
-            <div className="w-full bg-amber-500 h-[788px]">
-              <header className="w-full p-10 bg-purple-200"></header>
-              <section>{children}</section>
-            </div>
-          </main>
-        </div>
+    <div className="max-w-screen min-h-screen bg-green-400 flex">
+      <TopNavBar defaultPage={defaultPage} />
+      {defaultPage && <ClientSidebar />}
+      <div className="flex justify-center bg-white-400 w-screen">
+        <main className="flex p-4 w-full justify-center mt-[3.688rem] max-w-[82vw]">
+          <div className="w-full bg-amber-500 h-[788px]">
+            <header className="w-full p-10 bg-purple-200"></header>
+            <section>{children}</section>
+          </div>
+        </main>
       </div>
-    </>
+    </div>
   );
 }
