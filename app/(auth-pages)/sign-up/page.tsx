353c1c488eeeb5e53d804aa22cac8fa4d5f2342e
Emil Djurson <emidj236@student.liu.se>
2025-03-25 18:20:36 +0100

    Update page.tsx

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -1,4 +1,4 @@
-import { SignUpAction } from "@/app/actions";
+import { SignInAction, SignInActionGoogle, SignUpAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/supabase-template/form-message";
 import { SubmitButton } from "@/components/supabase-template/submit-button";
 import { Input } from "@/components/ui/input";
@@ -6,6 +6,10 @@ import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { SmtpMessage } from "../smtp-message";
 import LogoCenter from "@/components/form/logocenter";
+import InputField from "@/components/form/inputfield";
+import { ChevronRight } from "lucide-react";
+import FormButton from "@/components/form/formbutton";
+import GoogleIcon from "@/components/googleIcon";
 
 export default async function Signup(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
@@ -28,33 +32,34 @@ export default async function Signup(props: { searchParams: Promise<Message> }) 
             <h1 className="font-semibold text-3xl">Skapa ett konto</h1>
             <p className="text-base font-normal text-center">
               Ett konto gör att vi kan erbjuda dig samma upplevelse på flera enheter. Har du redan ett konto?
-              <Link href={"/login"} className="text-blue-900">
+              <Link href={"/login"} className="ml-2 text-blue-900">
                 Logga in
               </Link>
             </p>
+            <form className="w-lg flex flex-col justify-center gap-4">
+              <FormMessage message={searchParams} />
+              <div className="flex w-full flex-col gap-4">
+                <InputField name="email" type="email" placeholder="E-post" required />
+                <InputField name="password" type="password" placeholder="Lösenord" required />
+                <Link href={"/forgot-password"}>
+                  <span className="text-blue-900 flex">
+                    Glömt ditt lösenord? <ChevronRight className="text-blue-900" />
+                  </span>
+                </Link>
+                <div className="flex flex-col gap-4 justify-center item-center">
+                  <FormButton formAction={SignInAction} type="submit">
+                    Logga in
+                  </FormButton>
+                  <FormButton className="!bg-white-100 !text-gray-600 border-2 border-gray-100" formAction={SignInActionGoogle}>
+                    <GoogleIcon />
+                    Logga in med Google
+                  </FormButton>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-      <form className="flex flex-col min-w-64 max-w-64">
-        <h1 className="text-2xl font-medium">Sign up</h1>
-        <p className="text-sm text text-foreground">
-          Already have an account?
-          <Link className="text-primary font-medium underline" href="/sign-in">
-            Sign in
-          </Link>
-        </p>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <Label htmlFor="password">Password</Label>
-          <Input type="password" name="password" placeholder="Your password" minLength={6} required />
-          <SubmitButton formAction={SignUpAction} pendingText="Signing up...">
-            Sign up
-          </SubmitButton>
-          <FormMessage message={searchParams} />
-        </div>
-      </form>
-      <SmtpMessage />
     </>
   );
 }
