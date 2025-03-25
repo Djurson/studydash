ada9e5fe8a55814b1b582019de3455731ca3f3c5
Emil Djurson <emidj236@student.liu.se>
2025-03-25 17:05:04 +0100

    Upgrade Next JS

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -7,9 +7,7 @@ import Link from "next/link";
 import { SmtpMessage } from "../smtp-message";
 import LogoCenter from "@/components/form/logocenter";
 
-export default async function Signup(props: {
-  searchParams: Promise<Message>;
-}) {
+export default async function Signup(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
   if ("message" in searchParams) {
     return (
@@ -22,18 +20,25 @@ export default async function Signup(props: {
   return (
     <>
       <div className="flex flex-col gap-12">
-        <Link href={"/"}><LogoCenter /></Link>
+        <Link href={"/"}>
+          <LogoCenter />
+        </Link>
         <div className="flex flex-col w-full justify-center items-center h-full gap-6">
           <div className="flex flex-col justify-center items-center gap-3 w-lg">
             <h1 className="font-semibold text-3xl">Skapa ett konto</h1>
-            <p className="text-base font-normal text-center">Ett konto gör att vi kan erbjuda dig samma upplevelse på flera enheter. Har du redan ett konto? <Link href={"/login"} className="text-blue-900">Logga in</Link></p>
+            <p className="text-base font-normal text-center">
+              Ett konto gör att vi kan erbjuda dig samma upplevelse på flera enheter. Har du redan ett konto?
+              <Link href={"/login"} className="text-blue-900">
+                Logga in
+              </Link>
+            </p>
           </div>
         </div>
       </div>
       <form className="flex flex-col min-w-64 max-w-64">
         <h1 className="text-2xl font-medium">Sign up</h1>
         <p className="text-sm text text-foreground">
-          Already have an account?{" "}
+          Already have an account?
           <Link className="text-primary font-medium underline" href="/sign-in">
             Sign in
           </Link>
@@ -42,13 +47,7 @@ export default async function Signup(props: {
           <Label htmlFor="email">Email</Label>
           <Input name="email" placeholder="you@example.com" required />
           <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            minLength={6}
-            required
-          />
+          <Input type="password" name="password" placeholder="Your password" minLength={6} required />
           <SubmitButton formAction={SignUpAction} pendingText="Signing up...">
             Sign up
           </SubmitButton>
