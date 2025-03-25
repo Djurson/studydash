121b9b49cd24a246ed51e8eac7cefd0d79971258
AntonMartic <anton.martic@gmail.com>
2025-03-25 14:33:52 +0100

    card layout dashboard

---
+++ components/card/card.tsx
@@ -0,0 +1,10 @@
+export default function Card() {
+  return (
+    <>
+      <main className="p-4 bg-white rounded-2xl shadow-[2px_4px_12px_0px_rgba(0,_0,_0,_0.08)]">
+        <header> test header</header>
+        <section> test content</section>
+      </main>
+    </>
+  );
+}
