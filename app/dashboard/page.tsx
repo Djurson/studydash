a16e1f4ca282d4d175ddb3f5b167ac3ae428f839
AntonMartic <anton.martic@gmail.com>
2025-03-25 14:33:52 +0100

    card layout dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,21 @@
+import Card from "@/components/card/card";
+
 export default function Page() {
   return (
     <>
-      <h1>Hej</h1>
+      <main className="w-full h-full bg-amber-200 grid grid-cols-5 grid-rows-2 gap-4">
+        <div className="row-span-2 col-span-2">
+          <Card />
+        </div>
+        <Card />
+        <Card />
+        <div className="row-span-2 ">
+          <Card />
+        </div>
+        <div className="col-span-2 ">
+          <Card />
+        </div>
+      </main>
     </>
   );
 }
