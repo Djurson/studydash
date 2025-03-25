635e50bd0b697a2be499507b28938b5448fad819
AntonMartic <anton.martic@gmail.com>
2025-03-25 16:39:20 +0100

    added header functionality

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,9 +1,32 @@
-export default function Card() {
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils"; // Utility for merging class names
+import { ReactNode } from "react";
+import { ChevronRight } from "lucide-react";
+
+type CardProps = {
+  cardTitle: string;
+  variant?: "default" | "compact" | "large";
+  children?: ReactNode;
+};
+
+// const variants = cva(
+//     "big": "text-base",
+//     "small": " text-sm"
+// );
+
+export default function Card({
+  variant = "default",
+  children,
+  cardTitle,
+}: CardProps) {
   return (
     <>
-      <main className="p-4 bg-white rounded-2xl shadow-[2px_4px_12px_0px_rgba(0,_0,_0,_0.08)]">
-        <header> test header</header>
-        <section> test content</section>
+      <main className="p-4 bg-white rounded-2xl shadow-[2px_4px_12px_0px_rgba(0,_0,_0,_0.08)] w-full h-full">
+        <header className="flex">
+          <p className="text-lg">{cardTitle}</p>
+          <ChevronRight size={24} />
+        </header>
+        <section> {children}</section>
       </main>
     </>
   );
