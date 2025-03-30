0760b2cfcd72e6775ca8a1d640c2621a61bedd6a
Emil Djurson <emidj236@student.liu.se>
2025-03-30 14:08:20 +0200

    Börjat lista ut lite

--- components/form/uploadpdf.tsx
+++ components/form/uploadpdf.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { ComponentProps, useState } from "react";
+import { ChangeEvent, ComponentProps, useState } from "react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { FileUp, Info } from "lucide-react";
 import Link from "next/link";
 import FormButton from "./formbutton";
+import { tryCatch } from "../utils/trycatch";
+import { HandleFileUpload } from "@/app/results/actions";
 
 export function UploadPDFInput({ ...props }: ComponentProps<typeof Input> & ComponentProps<typeof Label>) {
   return (
@@ -24,8 +26,25 @@ export function UploadPDFInput({ ...props }: ComponentProps<typeof Input> & Comp
       <Link href={"/"}>
         <Info className="absolute w-6 h-6 text-gray-600 cursor-pointer top-4 right-4 aspect-square" />
       </Link>
-      <Input type="file" accept=".pdf" name="PDF-Upload" id="PDF-Upload" className="hidden appearance-none" {...props} />
-      <FormButton>Ladda upp</FormButton>
+      <Input type="file" accept=".pdf" name="PDF-Upload" id="PDF-Upload" className="hidden appearance-none" {...props} onChange={HandleFileInput} />
+      {/* <FormButton>Ladda upp</FormButton> */}
     </>
   );
+}
+
+async function HandleFileInput(e: ChangeEvent<HTMLInputElement>) {
+  e.preventDefault();
+
+  const file = e.target.files?.[0];
+  const fileName = file?.name;
+
+  if (!file) return;
+  if (!fileName?.toLowerCase().endsWith(".pdf")) return;
+
+  const formData = new FormData();
+  formData.append("file", file);
+
+  HandleFileUpload(file).then((res) => {
+    console.log(res);
+  });
 }
