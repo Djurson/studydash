e1729a80a04822df6a0d35e756f3a2e5442a9017
Emil Djurson <emil@djurson.se>
2025-03-31 16:51:13 +0200

    Dokumentation

--- components/PageTemplate.tsx
+++ components/PageTemplate.tsx
@@ -7,6 +7,20 @@ type PageProps = {
   defaultPage: boolean;
 };
 
+
+/**
+ * Page template for the main pages
+ * 
+ * @remarks
+ * This component must take in the default page boolean
+ * 
+ * @param children - Children components and tags, could be html tags <div>, <p>, etc or components
+ * 
+ * @param defaultPage - Determines the page layout, either true or false, check figma design for layouts
+ * 
+ * @returns Returns the page layout with sidenavigation and top navigation
+ * 
+*/
 export function PageTemplate({ children, defaultPage }: PageProps) {
   return (
     <div className="max-w-screen min-h-screen bg-green-400 flex">
