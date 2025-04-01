13282537c20963d8a9c4855cbb37ff3dabc09ee5
AntonMartic <anton.martic@gmail.com>
2025-04-01 23:14:35 +0200

    styling and (fetching json directly)

--- components/accordions/SemesterAccordion.tsx
+++ components/accordions/SemesterAccordion.tsx
@@ -18,16 +18,17 @@ export default function SemesterAccordion({
 
   return (
     <main className="p-4 bg-white rounded-2xl shadow-[2px_4px_12px_0px_rgba(0,_0,_0,_0.08)] w-full h-full">
-      <button className="flex" onClick={() => setIsOpen(!isOpen)}>
-        <div className="border-1 border-gray-900 rounded-s-sm"></div>
+      <button className="flex items-center" onClick={() => setIsOpen(!isOpen)}>
+        <div className="border-1 border-gray-900 rounded-sm w-[1.188rem] aspect-square"></div>
         <h3>{semester.name}</h3>
         <p>HT 2022</p>
         <p>0/30 hp</p>
-        {isOpen ? (
-          <ChevronDown size={24} rotate={180} />
-        ) : (
-          <ChevronDown size={24} />
-        )}
+        <ChevronDown
+          size={24}
+          className={`transition-transform duration-200 ease-in-out ${
+            isOpen ? "rotate-180" : "rotate-0"
+          }`}
+        />
       </button>
       <section></section>
     </main>
