ea008eeac7c261e1313a8027f8e3eb0d0b607640
AntonMartic <anton.martic@gmail.com>
2025-04-02 17:04:00 +0200

    fixed some grids

--- components/accordions/SemesterAccordion.tsx
+++ components/accordions/SemesterAccordion.tsx
@@ -28,20 +28,24 @@ export default function SemesterAccordion({
   return (
     <main className="p-4 bg-white rounded-2xl shadow-[2px_4px_12px_0px_rgba(0,_0,_0,_0.08)] w-full h-full">
       <button
-        className="flex justify-between items-center w-full"
+        className="grid grid-cols-10 grid-rows-1 items-center w-full"
         onClick={() => setIsOpen(!isOpen)}>
-        <div className="flex gap-4 items-center">
+        <div className="col-start-1 col-span-5 flex gap-4 items-center">
           <div className="border-1 border-gray-900 rounded-sm h-[1.188rem] aspect-square"></div>
           <h3 className="text-xl font-medium">{semester.name.slice(0, -7)}</h3>
         </div>
 
-        <p className="text-xs text-gray-600">{semester.name.slice(-7)}</p>
+        <p className="col-start-6 col-span-2 text-xs text-gray-600 text-left">
+          {semester.name.slice(-7)}
+        </p>
 
-        <p className="text-xs text-gray-600">0/30 hp</p>
+        <p className="col-start-8 col-span-1 text-xs text-gray-600 text-left">
+          0/30 hp
+        </p>
 
         <ChevronDown
           size={24}
-          className={`transition-transform duration-200 ease-in-out ${
+          className={`col-start-10 justify-self-end transition-transform duration-200 ease-in-out ${
             isOpen ? "rotate-180" : "rotate-0"
           }`}
         />
