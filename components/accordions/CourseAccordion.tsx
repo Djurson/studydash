211544ce46ae93d50af626c0cacbb3eff23451fa
AntonMartic <anton.martic@gmail.com>
2025-04-02 17:04:00 +0200

    fixed some grids

--- components/accordions/CourseAccordion.tsx
+++ components/accordions/CourseAccordion.tsx
@@ -13,29 +13,31 @@ export default function CourseAccordion({ course }: { course: Course }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="overflow-hidden pl-4">
+    <div className="overflow-hidden ">
       <button
-        className="w-full text-left flex justify-between items-center py-2"
+        className="grid grid-cols-10 grid-rows-1 w-full text-left items-center py-2"
         onClick={() => setIsOpen(!isOpen)}>
-        <div className="flex items-center justify-between w-full">
-          <div className="flex gap-4 items-center">
-            <div className="border-1 border-gray-900 rounded-sm h-[1rem] aspect-square"></div>
-            <h4 className="font-medium text-sm">
-              {course.name} - {course.course_code}
-            </h4>
-          </div>
-
-          <p className="text-xs text-gray-600">0/{course.credits}</p>
-          <div className="h-[1.625rem] w-[5.375rem] bg-emerald-200 rounded-2xl items-center justify-center">
-            <p className="text-center">Betyg 5</p>
-          </div>
-          <ChevronDown
-            size={24}
-            className={`transition-transform duration-200 ease-in-out ${
-              isOpen ? "rotate-180" : "rotate-0"
-            }`}
-          />
+        <div className="col-start-1 col-span-5 pl-4 flex gap-4 items-center">
+          <div className="border-1 border-gray-900 rounded-sm h-[1rem] aspect-square"></div>
+          <h4 className="font-medium text-sm">
+            {course.name} - {course.course_code}
+          </h4>
         </div>
+        <p className="col-start-6 col-span-2 text-xs text-gray-600">
+          xxxx-xx-xx
+        </p>
+        <p className="col-start-8 col-span-1 text-xs text-gray-600">
+          0/{course.credits}
+        </p>
+        <div className="h-[1.625rem] w-[5.375rem] bg-emerald-200 rounded-2xl items-center justify-center">
+          <p className="text-center">Betyg 5</p>
+        </div>
+        <ChevronDown
+          size={24}
+          className={`col-start-10 justify-self-end transition-transform duration-200 ease-in-out ${
+            isOpen ? "rotate-180" : "rotate-0"
+          }`}
+        />
       </button>
 
       {isOpen && (
@@ -50,7 +52,9 @@ export default function CourseAccordion({ course }: { course: Course }) {
           </div>
         </div>
       )}
-      <Separator />
+      <div className="pl-4">
+        <Separator />
+      </div>
     </div>
   );
 }
