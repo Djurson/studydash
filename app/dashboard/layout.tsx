a9a59ecae0c8ff880e4b69a09d63f2d80caf3221
Emil Djurson <emil@djurson.se>
2025-04-07 18:50:38 +0200

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,5 @@
 import { PageTemplate } from "@/components/PageTemplate";
 import { AuthProvider } from "@/components/supabase/authprovider";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 export default async function DashboardLayout({ children }: { children: ReactNode }) {
