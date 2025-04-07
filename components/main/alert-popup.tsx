7aa766a677b020a9947fc21d5850fce1b477bccc
Emil Djurson <emidj236@student.liu.se>
2025-04-07 17:31:47 +0200

    Lösning över ifall det inte finns kursdata för en person

--- components/main/alert-popup.tsx
+++ components/main/alert-popup.tsx
@@ -1,56 +1,56 @@
+import Link from "next/link";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "../ui/alert-dialog"
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
 
 type AlertPopupWindowProps = {
-    defaultOpen?: boolean;
-    title: string;
-    description: string;
-    actiontext: string;
-    actionlink?: string;
-}
+  defaultOpen?: boolean;
+  title: string;
+  description: string;
+  actiontext: string;
+  actionlink?: string;
+};
 
 /**
  * Alert popup window component
- * 
+ *
  * @remarks
  * This component renders an alert dialog window with a customizable title, description,
  * and action text. It allows for a default open state and an optional action link.
- * 
+ *
  * @param defaultOpen - Optional boolean to determine if the dialog is open by default
  * @param title - The title text for the alert dialog
  * @param description - The description text for the alert dialog
  * @param actiontext - The text to display on the action button
  * @param actionlink - Optional URL that the action button can link to
- * 
+ *
  * @returns Returns an alert dialog with the specified content and action button
  */
 
-
 export default function AlertPopupWindow({ defaultOpen, title, description, actiontext, actionlink }: AlertPopupWindowProps) {
-    return (
-        <>
-            <AlertDialog defaultOpen={defaultOpen ?? false}>
-                <AlertDialogContent>
-                    <AlertDialogHeader>
-                        <AlertDialogTitle>{title}</AlertDialogTitle>
-                        <AlertDialogDescription>
-                            {description}
-                        </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                        <AlertDialogAction>{actiontext}</AlertDialogAction>
-                    </AlertDialogFooter>
-                </AlertDialogContent>
-            </AlertDialog >
-        </>
-    )
+  return (
+    <>
+      <AlertDialog defaultOpen={defaultOpen ?? false}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{title}</AlertDialogTitle>
+            <AlertDialogDescription>{description}</AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <Link href={actionlink ?? "#"}>
+              <AlertDialogAction>{actiontext}</AlertDialogAction>
+            </Link>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
 }
