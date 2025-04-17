c75b32c955ae27b8a550ac2ac8ab2068a6051f0b
AntonMartic <anton.martic@gmail.com>
2025-04-17 17:40:19 +0200

    credits chart

--- components/charts/credits.tsx
+++ components/charts/credits.tsx
@@ -7,116 +7,90 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Content } from "next/font/google";
-const chartData = [
-  { date: "2024-04-01", desktop: 222, mobile: 150 },
-  { date: "2024-04-02", desktop: 97, mobile: 180 },
-  { date: "2024-04-03", desktop: 167, mobile: 120 },
-  { date: "2024-04-04", desktop: 242, mobile: 260 },
-  { date: "2024-04-05", desktop: 373, mobile: 290 },
-  { date: "2024-04-06", desktop: 301, mobile: 340 },
-  { date: "2024-04-07", desktop: 245, mobile: 180 },
-  { date: "2024-04-08", desktop: 409, mobile: 320 },
-  { date: "2024-04-09", desktop: 59, mobile: 110 },
-  { date: "2024-04-10", desktop: 261, mobile: 190 },
-  { date: "2024-04-11", desktop: 327, mobile: 350 },
-  { date: "2024-04-12", desktop: 292, mobile: 210 },
-  { date: "2024-04-13", desktop: 342, mobile: 380 },
-  { date: "2024-04-14", desktop: 137, mobile: 220 },
-  { date: "2024-04-15", desktop: 120, mobile: 170 },
-  { date: "2024-04-16", desktop: 138, mobile: 190 },
-  { date: "2024-04-17", desktop: 446, mobile: 360 },
-  { date: "2024-04-18", desktop: 364, mobile: 410 },
-  { date: "2024-04-19", desktop: 243, mobile: 180 },
-  { date: "2024-04-20", desktop: 89, mobile: 150 },
-  { date: "2024-04-21", desktop: 137, mobile: 200 },
-  { date: "2024-04-22", desktop: 224, mobile: 170 },
-  { date: "2024-04-23", desktop: 138, mobile: 230 },
-  { date: "2024-04-24", desktop: 387, mobile: 290 },
-  { date: "2024-04-25", desktop: 215, mobile: 250 },
-  { date: "2024-04-26", desktop: 75, mobile: 130 },
-  { date: "2024-04-27", desktop: 383, mobile: 420 },
-  { date: "2024-04-28", desktop: 122, mobile: 180 },
-  { date: "2024-04-29", desktop: 315, mobile: 240 },
-  { date: "2024-04-30", desktop: 454, mobile: 380 },
-  { date: "2024-05-01", desktop: 165, mobile: 220 },
-  { date: "2024-05-02", desktop: 293, mobile: 310 },
-  { date: "2024-05-03", desktop: 247, mobile: 190 },
-  { date: "2024-05-04", desktop: 385, mobile: 420 },
-  { date: "2024-05-05", desktop: 481, mobile: 390 },
-  { date: "2024-05-06", desktop: 498, mobile: 520 },
-  { date: "2024-05-07", desktop: 388, mobile: 300 },
-  { date: "2024-05-08", desktop: 149, mobile: 210 },
-  { date: "2024-05-09", desktop: 227, mobile: 180 },
-  { date: "2024-05-10", desktop: 293, mobile: 330 },
-  { date: "2024-05-11", desktop: 335, mobile: 270 },
-  { date: "2024-05-12", desktop: 197, mobile: 240 },
-  { date: "2024-05-13", desktop: 197, mobile: 160 },
-  { date: "2024-05-14", desktop: 448, mobile: 490 },
-  { date: "2024-05-15", desktop: 473, mobile: 380 },
-  { date: "2024-05-16", desktop: 338, mobile: 400 },
-  { date: "2024-05-17", desktop: 499, mobile: 420 },
-  { date: "2024-05-18", desktop: 315, mobile: 350 },
-  { date: "2024-05-19", desktop: 235, mobile: 180 },
-  { date: "2024-05-20", desktop: 177, mobile: 230 },
-  { date: "2024-05-21", desktop: 82, mobile: 140 },
-  { date: "2024-05-22", desktop: 81, mobile: 120 },
-  { date: "2024-05-23", desktop: 252, mobile: 290 },
-  { date: "2024-05-24", desktop: 294, mobile: 220 },
-  { date: "2024-05-25", desktop: 201, mobile: 250 },
-  { date: "2024-05-26", desktop: 213, mobile: 170 },
-  { date: "2024-05-27", desktop: 420, mobile: 460 },
-  { date: "2024-05-28", desktop: 233, mobile: 190 },
-  { date: "2024-05-29", desktop: 78, mobile: 130 },
-  { date: "2024-05-30", desktop: 340, mobile: 280 },
-  { date: "2024-05-31", desktop: 178, mobile: 230 },
-  { date: "2024-06-01", desktop: 178, mobile: 200 },
-  { date: "2024-06-02", desktop: 470, mobile: 410 },
-  { date: "2024-06-03", desktop: 103, mobile: 160 },
-  { date: "2024-06-04", desktop: 439, mobile: 380 },
-  { date: "2024-06-05", desktop: 88, mobile: 140 },
-  { date: "2024-06-06", desktop: 294, mobile: 250 },
-  { date: "2024-06-07", desktop: 323, mobile: 370 },
-  { date: "2024-06-08", desktop: 385, mobile: 320 },
-  { date: "2024-06-09", desktop: 438, mobile: 480 },
-  { date: "2024-06-10", desktop: 155, mobile: 200 },
-  { date: "2024-06-11", desktop: 92, mobile: 150 },
-  { date: "2024-06-12", desktop: 492, mobile: 420 },
-  { date: "2024-06-13", desktop: 81, mobile: 130 },
-  { date: "2024-06-14", desktop: 426, mobile: 380 },
-  { date: "2024-06-15", desktop: 307, mobile: 350 },
-  { date: "2024-06-16", desktop: 371, mobile: 310 },
-  { date: "2024-06-17", desktop: 475, mobile: 520 },
-  { date: "2024-06-18", desktop: 107, mobile: 170 },
-  { date: "2024-06-19", desktop: 341, mobile: 290 },
-  { date: "2024-06-20", desktop: 408, mobile: 450 },
-  { date: "2024-06-21", desktop: 169, mobile: 210 },
-  { date: "2024-06-22", desktop: 317, mobile: 270 },
-  { date: "2024-06-23", desktop: 480, mobile: 530 },
-  { date: "2024-06-24", desktop: 132, mobile: 180 },
-  { date: "2024-06-25", desktop: 141, mobile: 190 },
-  { date: "2024-06-26", desktop: 434, mobile: 380 },
-  { date: "2024-06-27", desktop: 448, mobile: 490 },
-  { date: "2024-06-28", desktop: 149, mobile: 200 },
-  { date: "2024-06-29", desktop: 103, mobile: 160 },
-  { date: "2024-06-30", desktop: 446, mobile: 400 },
-];
+
+// funktion för att generera nuvarande studieåret
+function getCurrentStudyYear(startYear: number) {
+  const now = new Date();
+  const currentYear = now.getFullYear();
+  const currentMonth = now.getMonth();
+
+  // Om nuvarande månad är mindre än före augusti (månad 7 (börjar på 0)), dra av 1 från året
+  const studyYear = currentMonth < 7 ? currentYear - 1 : currentYear;
+
+  return {
+    current: `${studyYear} - ${studyYear + 1}`,
+    start: new Date(studyYear, 7, 1),
+    end: new Date(studyYear + 1, 6, 31),
+  };
+}
+
+// funktion för att generera alla studieår från ett startår för att kunna mappa i select dropdownen och visa timeframe
+function getAllStudyYears(startYear: number) {
+  const now = new Date();
+  const curretYear = now.getFullYear();
+  const currentMonth = now.getMonth();
+  const years = [];
+  const endYear = currentMonth >= 7 ? curretYear : curretYear - 1;
+  let yearsCount = 0;
+
+  for (let year = startYear; year <= endYear; year++) {
+    years.push({
+      year: `${year} - ${year + 1}`,
+      start: new Date(year, 7, 1),
+      end: new Date(year + 1, 7, 0),
+      counter: yearsCount++,
+    });
+  }
+
+  return years;
+}
+
+// funktion för att visa det korrekta månads och års timefrmen som datan visar
+function formatedTimeRange(range: string, startYear: number): string {
+  const now = new Date();
+
+  if (range === "90d") {
+    const startDate = new Date(now);
+    startDate.setDate(now.getDate() - 90);
+    return `${startDate.toLocaleDateString("sv-SE", { month: "long" })} ${startDate.getFullYear()} - ${now.toLocaleDateString("sv-SE", { month: "long" })} ${now.getFullYear()}`;
+  } else if (range === "180d") {
+    const startDate = new Date(now);
+    startDate.setDate(now.getDate() - 180);
+    return `${startDate.toLocaleDateString("sv-SE", { month: "long" })} ${startDate.getFullYear()} - ${now.toLocaleDateString("sv-SE", { month: "long" })} ${now.getFullYear()}`;
+  } else if (range === "Alla") {
+    return ` augusti ${startYear} - nu`;
+  } else {
+    const [startYear, endYear] = range.split(" - ").map(Number);
+    return ` augusti ${startYear} - juli ${endYear}`;
+  }
+}
+
+//funktion för att summera hp
+function calculateTotalCredits(filteredData: Array<{ credits: number }>): number {
+  return filteredData.reduce((total, item) => total + item.credits, 0);
+}
+
+// datum för hp är inte sorterade i stigande ordning i databasen,
+// TODO: göra en funktion för att sortera datumen i stigande ordning
+
+// Prop data:
 
 const submoduleCreditsData = [
+  { date: "2022-10-07", credits: 2.0 },
   { date: "2022-10-18", credits: 1.5 },
+  { date: "2022-10-25", credits: 1.0 },
   { date: "2022-10-26", credits: 4.5 },
-  { date: "2023-01-25", credits: 3.0 },
-  { date: "2023-01-28", credits: 3.0 },
+  { date: "2022-12-14", credits: 2.0 },
   { date: "2023-01-10", credits: 6.0 },
   { date: "2023-01-12", credits: 3.0 },
-  { date: "2022-10-07", credits: 2.0 },
-  { date: "2022-10-25", credits: 1.0 },
-  { date: "2022-12-14", credits: 2.0 },
   { date: "2023-01-13", credits: 1.0 },
-  { date: "2023-03-20", credits: 6.0 },
-  { date: "2023-03-16", credits: 3.0 },
-  { date: "2023-03-22", credits: 3.0 },
+  { date: "2023-01-25", credits: 3.0 },
+  { date: "2023-01-28", credits: 3.0 },
   { date: "2023-02-10", credits: 1.0 },
+  { date: "2023-03-16", credits: 3.0 },
   { date: "2023-03-19", credits: 1.0 },
+  { date: "2023-03-20", credits: 6.0 },
+  { date: "2023-03-22", credits: 3.0 },
   { date: "2023-05-07", credits: 1.0 },
   { date: "2023-05-07", credits: 0.5 },
   { date: "2023-05-07", credits: 0.5 },
@@ -124,39 +98,39 @@ const submoduleCreditsData = [
   { date: "2023-06-01", credits: 6.0 },
   { date: "2023-06-02", credits: 3.0 },
   { date: "2023-06-07", credits: 3.0 },
-  { date: "2023-10-28", credits: 6.0 },
+  { date: "2023-08-31", credits: 1.0 },
   { date: "2023-10-16", credits: 1.0 },
   { date: "2023-10-24", credits: 4.0 },
-  { date: "2023-11-20", credits: 1.0 },
-  { date: "2023-08-31", credits: 1.0 },
+  { date: "2023-10-28", credits: 6.0 },
   { date: "2023-10-30", credits: 2.0 },
+  { date: "2023-11-20", credits: 1.0 },
+  { date: "2023-12-20", credits: 3.0 },
   { date: "2023-12-20", credits: 3.0 },
   { date: "2024-01-08", credits: 6.0 },
-  { date: "2023-12-20", credits: 3.0 },
   { date: "2024-01-10", credits: 3.0 },
-  { date: "2024-03-05", credits: 1.5 },
-  { date: "2024-03-23", credits: 4.5 },
   { date: "2024-03-04", credits: 1.0 },
+  { date: "2024-03-05", credits: 1.5 },
+  { date: "2024-03-07", credits: 1.5 },
   { date: "2024-03-22", credits: 4.0 },
+  { date: "2024-03-23", credits: 4.5 },
   { date: "2024-03-28", credits: 0.5 },
   { date: "2024-03-28", credits: 0.5 },
-  { date: "2024-05-31", credits: 6.0 },
   { date: "2024-04-25", credits: 2.0 },
   { date: "2024-05-29", credits: 2.0 },
-  { date: "2024-06-18", credits: 2.0 },
-  { date: "2024-03-07", credits: 1.5 },
+  { date: "2024-05-31", credits: 6.0 },
   { date: "2024-06-01", credits: 1.5 },
+  { date: "2024-06-18", credits: 2.0 },
   { date: "2024-07-08", credits: 3.0 },
   { date: "2024-10-21", credits: 1.5 },
+  { date: "2024-10-24", credits: 1.5 },
+  { date: "2024-10-24", credits: 1.5 },
   { date: "2024-10-25", credits: 4.5 },
-  { date: "2024-10-24", credits: 1.5 },
-  { date: "2024-10-24", credits: 1.5 },
   { date: "2024-10-29", credits: 3.0 },
-  { date: "2024-12-21", credits: 1.5 },
-  { date: "2025-01-13", credits: 4.5 },
   { date: "2024-12-17", credits: 0.5 },
   { date: "2024-12-19", credits: 0.5 },
   { date: "2024-12-19", credits: 1.0 },
+  { date: "2024-12-21", credits: 1.5 },
+  { date: "2025-01-13", credits: 4.5 },
   { date: "2025-01-17", credits: 4.0 },
   { date: "2025-01-19", credits: 6.0 },
   { date: "2025-03-27", credits: 2.0 },
@@ -168,75 +142,86 @@ const chartConfig = {
   visitors: {
     label: "Visitors",
   },
-  desktop: {
-    label: "Desktop",
+  credits: {
+    label: "credits",
     color: "hsl(var(--chart-1))",
-  },
-  mobile: {
-    label: "Mobile",
-    color: "hsl(var(--chart-2))",
   },
 } satisfies ChartConfig;
 
 export function Credits() {
-  const [timeRange, setTimeRange] = React.useState("90d");
+  // läsa in startdatum från databasen (exempelvis 2022) och ta året och lägga till 08-01
+  const startYear = 2022; // hårdkodat för tillfället
+  const studyYears = getAllStudyYears(startYear);
+  const currentStudyYear = getCurrentStudyYear(startYear);
 
-  const filteredData = chartData.filter((item) => {
+  const [timeRange, setTimeRange] = React.useState<string>(currentStudyYear.current);
+
+  const filteredData = submoduleCreditsData.filter((item) => {
     const date = new Date(item.date);
-    const referenceDate = new Date("2024-06-30");
-    let daysToSubtract = 90;
-    if (timeRange === "30d") {
-      daysToSubtract = 30;
-    } else if (timeRange === "7d") {
-      daysToSubtract = 7;
+    const credits = item.credits;
+
+    if (timeRange === "90d") {
+      const refferenceDate = new Date();
+      const startDate = new Date(refferenceDate);
+      startDate.setDate(startDate.getDate() - 90);
+      return date >= startDate;
+    } else if (timeRange === "180d") {
+      const refferenceDate = new Date();
+      const startDate = new Date(refferenceDate);
+      startDate.setDate(startDate.getDate() - 180);
+      return date >= startDate;
+    } else if (timeRange === "Alla") {
+      return true;
+    } else {
+      const selectedYear = studyYears.find((year) => year.year === timeRange);
+      if (selectedYear) {
+        return date >= selectedYear.start && date <= selectedYear.end;
+      }
+      return false;
     }
-    const startDate = new Date(referenceDate);
-    startDate.setDate(startDate.getDate() - daysToSubtract);
-    return date >= startDate;
   });
 
   return (
     <main>
-      <header className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
-        <div className="grid flex-1 gap-1 text-center sm:text-left">
-          <CardTitle>Area Chart - Interactive</CardTitle>
-          <CardDescription>Showing total visitors for the last 3 months</CardDescription>
+      <header className="py-4 flex flex-col gap-4">
+        <div className="flex justify-between items-center">
+          <p className="text-sm text-muted-foreground"> {formatedTimeRange(timeRange, startYear)}</p>
+          <Select value={timeRange} onValueChange={setTimeRange}>
+            <SelectTrigger className="w-[160px] rounded-lg sm:ml-auto" aria-label="Select a value">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent className="rounded-xl">
+              <SelectItem value="90d" className="rounded-lg">
+                3 månader
+              </SelectItem>
+              <SelectItem value="180d" className="rounded-lg">
+                6 månader
+              </SelectItem>
+              {studyYears.map((studyYear) => (
+                <SelectItem key={studyYear.year} value={studyYear.year} className="rounded-lg">
+                  Årskurs {studyYear.counter + 1}
+                </SelectItem>
+              ))}
+              <SelectItem value="Alla" className="rounded-lg">
+                Alla
+              </SelectItem>
+            </SelectContent>
+          </Select>
         </div>
-        <Select value={timeRange} onValueChange={setTimeRange}>
-          <SelectTrigger className="w-[160px] rounded-lg sm:ml-auto" aria-label="Select a value">
-            <SelectValue placeholder="Last 3 months" />
-          </SelectTrigger>
-          <SelectContent className="rounded-xl">
-            <SelectItem value="90d" className="rounded-lg">
-              Last 3 months
-            </SelectItem>
-            <SelectItem value="30d" className="rounded-lg">
-              Last 30 days
-            </SelectItem>
-            <SelectItem value="7d" className="rounded-lg">
-              Last 7 days
-            </SelectItem>
-            <SelectItem value="7d" className="rounded-lg">
-              Last 7 days
-            </SelectItem>
-            <SelectItem value="Alla" className="rounded-lg">
-              Alla
-            </SelectItem>
-          </SelectContent>
-        </Select>
+
+        <span className="text-4xl font-semibold flex items-end gap-2">
+          {calculateTotalCredits(filteredData)}
+          <p className="text-sm text-muted-foreground font-normal text-end"> totalt</p>
+        </span>
       </header>
 
       <section className="">
         <ChartContainer config={chartConfig} className="aspect-auto h-[250px] w-full">
           <AreaChart data={filteredData}>
             <defs>
-              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="var(--color-desktop)" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="var(--color-desktop)" stopOpacity={0.1} />
-              </linearGradient>
-              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="var(--color-mobile)" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="var(--color-mobile)" stopOpacity={0.1} />
+              <linearGradient id="fillCredits" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="var(--color-blue-900)" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="var(--color-blue-900)" stopOpacity={0.1} />
               </linearGradient>
             </defs>
 
@@ -249,20 +234,21 @@ export function Credits() {
               minTickGap={32}
               tickFormatter={(value) => {
                 const date = new Date(value);
-                return date.toLocaleDateString("en-US", {
+                return date.toLocaleDateString("sv-SE", {
                   month: "short",
                   day: "numeric",
                 });
               }}
             />
 
-            <YAxis width={40} tickLine={false} axisLine={false} tickMargin={2} tickFormatter={(value) => `${value} hp`} />
+            <YAxis width={40} tickLine={false} axisLine={false} tickMargin={8} tickFormatter={(value) => `${value} hp`} />
             <ChartTooltip
               cursor={false}
               content={
                 <ChartTooltipContent
                   labelFormatter={(value) => {
-                    return new Date(value).toLocaleDateString("en-US", {
+                    return new Date(value).toLocaleDateString("sv-SE", {
+                      year: "numeric",
                       month: "short",
                       day: "numeric",
                     });
@@ -271,9 +257,7 @@ export function Credits() {
                 />
               }
             />
-            <Area dataKey="mobile" type="natural" fill="url(#fillMobile)" stroke="var(--color-mobile)" stackId="a" />
-            <Area dataKey="desktop" type="natural" fill="url(#fillDesktop)" stroke="var(--color-desktop)" stackId="a" />
-            <ChartLegend content={<ChartLegendContent />} />
+            <Area dataKey="credits" type="natural" fill="url(#fillCredits)" stroke="var(--color-blue-900)" stackId="a" strokeWidth={1.5} />
           </AreaChart>
         </ChartContainer>
       </section>
