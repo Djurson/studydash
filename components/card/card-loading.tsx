4bdb6ee39dee43164f04bbfd83c601f35682435f
Emil Djurson <emil@djurson.se>
2025-04-19 20:21:36 +0200

    Lagt till loading osv

---
+++ components/card/card-loading.tsx
@@ -0,0 +1,44 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils"; // Utility for merging class names
+import { ReactNode } from "react";
+import { ChevronRight } from "lucide-react";
+import { Skeleton } from "../ui/skeleton";
+
+type CardProps = {
+  cardTitle: string;
+  variant?: "default" | "compact" | "large" | "header" | "no-header";
+  children?: ReactNode;
+};
+
+// const variants = cva(
+//     "big": "text-base",
+//     "small": " text-sm"
+// );
+
+/**
+ * A flexible card component that can display different types of content with optional title and variant styling.
+ *
+ * @param cardTitle - The title to be displayed at the top of the card.
+ * @param variant - Defines the card's style variant. Can be "default", "compact", "large", "header", or "no-header".
+ * @param children - The content to be displayed inside the card.
+ *
+ * @returns A styled card with optional header and variant styles, containing the provided children content.
+ *
+ * @example
+ * <Card cardTitle="Example Title" variant="large">
+ *   <p>This is an example of the card content.</p>
+ * </Card>
+ */
+
+export default function CardLoading({ children }: { children?: ReactNode }) {
+  return (
+    <>
+      <main className="p-4 bg-card rounded-2xl shadow-[2px_4px_12px_0px_rgba(0,_0,_0,_0.04)] w-full h-full duration-300 hover:scale-102 hover:shadow-[2px_4px_12px_0px_rgba(0,_0,_0,_0.1)] flex flex-col justify-between items-start gap-4">
+        <header className="flex items-center cursor-pointer hover:underline w-full h-fit">
+          <Skeleton className="w-4/5 h-6" />
+        </header>
+        <section className="w-full h-11/12">{children ? children : <Skeleton className="w-full h-full" />}</section>
+      </main>
+    </>
+  );
+}
--- serverhooks/withAuth.tsx
+++ serverhooks/withAuth.tsx
@@ -2,18 +2,34 @@ import { UserData, WithAuthProps } from "@/utils/types";
 import { getUserCache, getUserRowCache } from "./cachehelpers";
 import { createClient } from "@/utils/supabase/server";
 import { jsonToStudyResults, sortMapAndExtractMerit } from "@/utils/converters";
+import { redirect } from "next/navigation";
+import AlertPopupWindow from "@/components/main/alert-popup";
 
 export function withAuth<P extends WithAuthProps>(WrappedComponent: React.ComponentType<P>) {
   return async function AuthenticatedComponent(props: Omit<P, keyof WithAuthProps>) {
     const supabase = await createClient();
 
-    // 1) Hämta session (cachad i 10 min)
+    // 1) Hämta session (cachad i 30 min)
     const user = await getUserCache(supabase);
+    if (!user) {
+      redirect("/");
+    }
 
-    // 2) Hämta användardata (cachad i 10 min per user.id)
-    let row;
-    if (user) {
-      row = await getUserRowCache(user?.id, supabase);
+    // 2) Hämta användardata (cachad i 30 min per user.id)
+    let row = await getUserRowCache(user.id, supabase);
+    if (!row) {
+      return (
+        <WrappedComponent {...(props as P)} user={user} userData={undefined}>
+          <AlertPopupWindow
+            open={true}
+            defaultOpen
+            title="Saknad studieinfo"
+            description="Det verkar som att vi saknar din studieinformation. Vänligen fyll i din studieinformation för att kunna använda tjänsten"
+            actiontext="Fyll i studieinfo"
+            actionlink="/results"
+          />
+        </WrappedComponent>
+      );
     }
 
     // 3) Mappa till props-form
