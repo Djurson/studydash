ef19bfd33b0987e9576092e12125b178c5dec6e8
Emil Djurson <emil@djurson.se>
2025-05-08 15:19:46 +0200

    Update progressCard.tsx

--- components/program/progressCard.tsx
+++ components/program/progressCard.tsx
@@ -1,56 +1,15 @@
-"use client";
+"use server";
 
 import { Progress } from "@/components/ui/progress";
-import React, { useMemo } from "react";
-import { WithAuthProps, Examination, Program } from "@/utils/types";
+import { WithAuthProps, Examination, Program, Course } from "@/utils/types";
 import LiuImg from "@/assets/liu.png";
 import { ExternalLink } from "lucide-react";
 
-export function ProgressCard({ userData, url, credits }: Partial<WithAuthProps> & Partial<Program>) {
-  const programCredits = useMemo(() => {
-    if (!credits) return 0;
-    const creditsNumber = parseInt(credits.replace(/\D/g, ""));
-    return isNaN(creditsNumber) ? 0 : creditsNumber;
-  }, [credits]);
-
-  const [progress, setProgress] = React.useState(0);
-
-  const earnedCredits = useMemo(() => {
-    if (!userData?.sortedDateMap) return 0;
-
-    let totalCredits = 0;
-
-    // konvertera Map entries till array av examinationer
-    const exams: Examination[] = [];
-    userData.sortedDateMap.forEach((examArray) => {
-      exams.push(...examArray);
-    });
-
-    // sortera hp efter perioderna
-    exams.forEach((exam) => {
-      const credits = exam.hp;
-      totalCredits += credits;
-    });
-
-    return totalCredits;
-  }, [userData?.sortedDateMap]);
-
-  // beräknar procent för vald period
-  const percentage = useMemo(() => {
-    // check så att båda är nummer och det intle blir division med 0
-    if (typeof earnedCredits !== "number" || typeof programCredits !== "number") {
-      return 0;
-    }
-    if (programCredits <= 0) return 0;
-
-    return Math.round((earnedCredits / programCredits) * 100);
-  }, [earnedCredits, programCredits]);
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => setProgress(percentage), 500);
-    return () => clearTimeout(timer);
-  }, [percentage]);
-
+export async function ProgressCard({ userData, url, credits }: Partial<WithAuthProps> & Partial<Program>) {
+  const merits = GetMerits(userData?.studyinfo);
+  const totalMerits = Number.parseInt(credits?.slice(0, 3) ?? "300");
+  const progress = merits / totalMerits;
+  const percentage = progress * 100;
   return (
     <main className="flex flex-col gap-2">
       <div className="flex items-center">
@@ -64,11 +23,46 @@ export function ProgressCard({ userData, url, credits }: Partial<WithAuthProps> 
         </a>
       </div>
       <div className="flex flex-col gap-2">
-        <Progress value={progress} className="w-[100%] h-4" />
+        <Progress value={percentage} className="w-[100%] h-4" color="bg-blue-900" />
         <p className="text-sm font-light text-muted">
-          {percentage}% avklarad | {earnedCredits}/{programCredits} hp
+          {percentage.toFixed(1)}% avklarad | {merits}/{totalMerits} hp
         </p>
       </div>
     </main>
   );
+}
+
+function GetMerits(map: Map<string, Course> | undefined): number {
+  if (!map) {
+    return 0;
+  }
+
+  let merits = 0;
+
+  for (const course_code of map.keys()) {
+    const course = map.get(course_code);
+
+    if (!course) {
+      continue;
+    }
+
+    if (course.grade !== "") {
+      merits += course.hp;
+      continue;
+    }
+
+    for (const exam_code of course.examinations.keys()) {
+      const exam = course.examinations.get(exam_code);
+
+      if (!exam) {
+        continue;
+      }
+
+      if (exam.grade !== "") {
+        merits += exam.hp;
+        continue;
+      }
+    }
+  }
+  return merits;
 }
