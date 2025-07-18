4b3bce3ffd01365ef0e35897bf0782f64c9eadbb
Jonas <jonal563@student.liu.se>
2025-07-18 12:03:33 +0200

    Update page.tsx

--- app/dashboard/masterbuilder/page.tsx
+++ app/dashboard/masterbuilder/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+// Set-ExecutionPolicy Unrestricted -Scope Process (För Jonas)
 import { useState, useMemo } from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
